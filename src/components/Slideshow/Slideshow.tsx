d8b00074748d8ab0ad0852eb4c56e76d97fe6477
Bradley Baysinger <bradleybaysinger@gmail.com>
2025-01-09 23:38:24 -0800

    Documentation.

--- src/components/Slideshow/Slideshow.tsx
+++ src/components/Slideshow/Slideshow.tsx
@@ -39,6 +39,8 @@ const defaultLabels: SlideshowLabels = {
  * @param restartDelay - Delay in milliseconds before restarting the slideshow
  * @param transitionResetDelay - Duration of transitions that controls the 'transitioning' state and class
  * @param classPrefix - Prefix for all globally-scoped classes to avoid naming conflicts in applications
+ * This is also so we don't need to rely on nesting, to avoid specificity/
+ * overriding issues and prevent bloating the outputed CSS. 
  * @param debug - Whether to show debugging information
  * @param labels - Custom labels for directional/stepper navigation buttons
  */
